bb8f4a26950a176ba0fb1471cecec687322c1861
MIRONTER <91906992+MIRONTER@users.noreply.github.com>
2025-02-21 11:58:55 +0300

    ToolsForRemasters

---
+++ ToolsForRemasters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class ToolsForRemasters
+{
+    public static void Main(string[] args)
+    {
+        string[] files = Directory.GetFiles(args[0]);
+
+        var groupedFiles = files
+            .Where(file =>
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                return fileName.Length > 40;
+            })
+            .GroupBy(file =>
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                return fileName.Substring(17, 15);
+            });
+
+        foreach (var group in groupedFiles.Where(g => g.Count() > 1))
+        {
+            if (group.Count() > 3)
+            {
+                Console.WriteLine("Need check: " + group.Key);
+                Directory.CreateDirectory(group.Key);
+                foreach (var file in group)
+                    File.Move(file, group.Key + "\\" + Path.GetFileName(file));
+            }
+            else
+            {
+                string? largestFile = group.OrderByDescending(f => new FileInfo(f).Length).FirstOrDefault();
+                foreach (var file in group)
+                    if (new FileInfo(largestFile).Length > new FileInfo(file).Length + 30000)
+                        File.Copy(largestFile, file, true);
+            }
+        }
+    }
+}
